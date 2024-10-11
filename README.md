e453a6bf4b387a7a23c10f2c0ed76eca689ba7f1
xiyueyezibile <panqilin@xiaohongshu.com>
2024-10-11 13:52:07 +0800

    chore: upgrade version to 1.0.8

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # nmg-manage
-npm 管理工具，快速切换 npm 源
+简易项目管理工具，快速切换 npm 源打开项目目录
 
 ## 安装
 ```bash
@@ -7,10 +7,24 @@ npm install nmg-manage -g
 ```
 
 ## 快速上手
+
+### 操纵 npm 源
 ```bash
 nmg ls # 查看设置的所有npm源
 nmg add <tag> <url> # 添加npm源
 nmg cur # 查看当前使用的npm源
 nmg use <tag> # 切换npm源
 nmg clean # 清除所有npm源
+```
+
+
+### 操纵仓库启动
+
+
+
+```bash
+nmg ls -r # 查看设置的所有仓库
+nmg add <tag> [url] -r # 添加仓库地址，默认当前目录
+nmg clean -r # 清除所有仓库
+nmg code <tag> -i [vscode/ws] # 打开仓库目录，需要配置vscode的code命令或者wbstorm的ws命令才可以使用，默认使用vscode，使用-i参数指定一次ws之后后面默认为对应的编译器
 ```
--- src/index.ts
+++ src/index.ts
@@ -6,73 +6,125 @@ import chalk from "chalk";
 import { exec } from "child_process";
 
 const __dirname = path.resolve();
-const saveFilePath = path.join(__dirname, "../data.json");
+const saveFilePath = path.join(__dirname, "data.json");
+const saveRepoPath = path.join(__dirname, "repo-data.json");
+const configPath = path.join(__dirname, "config.json");
 
 program.version(pkg.version);
 program.description(pkg.description);
 program.name(pkg.name);
 program.usage("<command> [options]");
-program
-  .command("add <tag> <npm-address>")
-  .description("Add a new npm address to the list")
-  .action(async (tag, npmAddress) => {
-    chalk.green("Add a new npm package to the list");
-    const isExistSaveFile = await fs.exists(saveFilePath);
-    if (isExistSaveFile) {
-      const data = await fs.readFile(saveFilePath);
-      const saveLists = JSON.parse(data.toString());
-      saveLists.push({
-        name: tag,
-        target: npmAddress,
-      });
 
-      await fs.writeFile(saveFilePath, `${JSON.stringify(saveLists)}`);
-    } else {
-      await fs.ensureFile(saveFilePath);
-      const list = [
-        {
-          name: tag,
-          target: npmAddress,
-        },
-      ];
-      await fs.writeFile(saveFilePath, `${JSON.stringify(list)}`);
+async function hasDiffTag(tag: string, savePath: string) {
+  const isExistSaveFile = await fs.exists(savePath);
+  if (isExistSaveFile) {
+    const data = await fs.readFile(savePath);
+    const saveLists = JSON.parse(data.toString());
+    for (const item of saveLists) {
+      if (item.name === tag) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+async function addJSONData(tag: string, address: string, filePath: string) {
+  const isExistSaveFile = await fs.exists(filePath);
+  if (isExistSaveFile) {
+    const data = await fs.readFile(filePath);
+    const saveLists = JSON.parse(data.toString());
+    saveLists.push({
+      name: tag,
+      target: address,
+    });
+
+    await fs.writeFile(filePath, `${JSON.stringify(saveLists)}`);
+  } else {
+    await fs.ensureFile(filePath);
+    const list = [
+      {
+        name: tag,
+        target: address,
+      },
+    ];
+    await fs.writeFile(filePath, `${JSON.stringify(list)}`);
+  }
+}
+
+program
+  .command("add <tag> [npm-address]")
+  .option("-r")
+  .description("Add a new npm address to the list")
+  .action(async (tag, npmAddress, options) => {
+    
+    if (options.r && (await hasDiffTag(tag, saveRepoPath))) {
+      await addJSONData(
+        tag,
+        npmAddress || path.join(process.cwd()),
+        saveRepoPath
+      );
+    } else if (!options.r && (await hasDiffTag(tag, saveFilePath))) {
+      if (!npmAddress) {
+        console.log(chalk.red("npm address is required"));
+        return;
+      }
+      await addJSONData(tag, npmAddress, saveFilePath);
     }
     console.log(chalk.green("Add success"));
   });
 program
   .command("ls")
+  .option("-r", "List all repo")
   .description("List all npm address")
-  .action(async () => {
-    const isExistSaveFile = await fs.exists(saveFilePath);
-    if (isExistSaveFile) {
-      console.log(chalk.green("npm list: "));
-      const data = await fs.readFile(saveFilePath);
-      const saveLists = JSON.parse(data.toString());
-
-      for (const item of saveLists) {
-        console.log(`${chalk.green(item.name)}(${item.target})`);
+  .action(async (options) => {
+    if (options.r) {
+      const isExistSaveFile = await fs.exists(saveRepoPath);
+      if (isExistSaveFile) {
+        console.log(chalk.green("repo list: "));
+        const data = await fs.readFile(saveRepoPath);
+        const saveLists = JSON.parse(data.toString());
+        for (const item of saveLists) {
+          console.log(`${chalk.green(item.name)}(${item.target})`);
+        }
+      } else {
+        console.log(chalk.red("No repo found"));
       }
-
-      //   const saveLists = JSON.parse(data);
-      //   console.log(saveLists);
     } else {
-      console.log(chalk.red("No lists found"));
+      const isExistSaveFile = await fs.exists(saveFilePath);
+      if (isExistSaveFile) {
+        console.log(chalk.green("npm list: "));
+        const data = await fs.readFile(saveFilePath);
+        const saveLists = JSON.parse(data.toString());
+
+        for (const item of saveLists) {
+          console.log(`${chalk.green(item.name)}(${item.target})`);
+        }
+
+        //   const saveLists = JSON.parse(data);
+        //   console.log(saveLists);
+      } else {
+        console.log(chalk.red("No lists found"));
+      }
     }
   });
 program
-  .command("clean [tag]")
+  .command("clean")
+  .option("-r")
   .description("Clean all npm address")
-  .action(async () => {
-    await fs.unlink(saveFilePath);
+  .action(async (options) => {
+    if (options.r) {
+      await fs.unlink(saveRepoPath);
+    } else {
+      await fs.unlink(saveFilePath);
+    }
     console.log(chalk.green("Clean All success"));
   });
 program
   .command("cur")
   .description("current npm address")
   .action(async () => {
-    const isExistConfigPath = await fs.exists(
-      path.join(__dirname, "config.json")
-    );
+    const isExistConfigPath = await fs.exists(configPath);
     if (!isExistConfigPath) {
       exec("npm config get registry", (err, stdout) => {
         if (err) {
@@ -82,7 +134,7 @@ program
         }
       });
     } else {
-      const data = await fs.readFile(path.join(__dirname, "config.json"));
+      const data = await fs.readFile(configPath);
       const config = JSON.parse(data.toString());
       console.log(
         `current address: ${chalk.green(config.name)}(${config.address})`
@@ -93,9 +145,7 @@ program
   .command("use <tag>")
   .description("use npm address")
   .action(async (tag) => {
-    const isExistConfigPath = await fs.exists(
-      path.join(__dirname, "config.json")
-    );
+    const isExistConfigPath = await fs.exists(configPath);
     const isExistSaveFile = await fs.exists(saveFilePath);
     if (!isExistSaveFile) {
       console.log(
@@ -114,29 +164,26 @@ program
     }
     if (!isExistConfigPath) {
       await fs.writeFile(
-        path.join(__dirname, "config.json"),
+        configPath,
         JSON.stringify({
           name: tag,
           address: savaLists.filter((item: any) => item.name === tag)[0].target,
         })
       );
     } else {
-      const configData = await fs.readFile(path.join(__dirname, "config.json"));
+      const configData = await fs.readFile(configPath);
       const config = JSON.parse(configData.toString());
       config.name = tag;
       config.address = savaLists.filter(
         (item: any) => item.name === tag
       )[0].target;
-      await fs.writeFile(
-        path.join(__dirname, "config.json"),
-        JSON.stringify(config)
-      );
+      await fs.writeFile(configPath, JSON.stringify(config));
     }
     exec(
       `npm config set registry ${
         savaLists.filter((item: any) => item.name === tag)[0].target
       }`,
-      (err,  stderr) => {
+      (err, stderr) => {
         if (err) {
           console.log(chalk.red(err));
           return;
@@ -152,5 +199,39 @@ program
         );
       }
     );
+  });
+program
+  .command("code <tag>")
+  .option("-i [idea]")
+  .description("open a repo")
+  .action(async (tag: string, options: any) => {
+    const isExistConfigPath = await fs.exists(configPath);
+    if (!isExistConfigPath) {
+      await fs.writeFile(
+        configPath,
+        JSON.stringify({ name: "", address: "", idea: options.i || "vscode" })
+      );
+    } else {
+      const configData = await fs.readFile(configPath);
+      const config = JSON.parse(configData.toString());
+      config.idea = options.i || "vscode";
+      await fs.writeFile(configPath, JSON.stringify(config));
+    }
+
+    const data = await fs.readFile(saveRepoPath);
+    const savaLists = JSON.parse(data.toString());
+    if (savaLists.filter((item: any) => item.name === tag).length === 0) {
+      console.log(chalk.red("tag not found"));
+      return;
+    }
+    if (options.i === "vscode") {
+      exec(
+        `code ${savaLists.filter((item: any) => item.name === tag)[0].target}`
+      );
+    } else {
+      exec(
+        `ws ${savaLists.filter((item: any) => item.name === tag)[0].target}`
+      );
+    }
   });
 program.parse(process.argv);
